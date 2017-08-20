fc56295743ea1cacbaca694320dd38a67070af69
Aditya Taneja <adityatnj21@gmail.com>
2017-08-20 06:04:34 -0400

    Snowboard Fighting

---
+++ SnowboardFighting/Assets/Shoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+    public Bullet bulletPrefab;
+	private int sensitivity = 10;
+
+	public Texture2D crosshairImage;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+  
+            Bullet clone = (Bullet)Instantiate(bulletPrefab, transform.position, transform.rotation);
+        }
+
+		if (Input.GetAxis ("Mouse Y") != 0) {
+			var y = Input.GetAxis ("Mouse Y") * sensitivity;
+			transform.eulerAngles -= new Vector3 (0, 0, y);
+		}
+	}
+
+	void OnGUI()
+	{
+		float xMin = (Screen.width / 2) - (crosshairImage.width / 4 / 2) - 32;
+		float yMin = (Screen.height / 2) - (crosshairImage.height / 4 / 2);
+		GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width/4, crosshairImage.height/4), crosshairImage);
+	}
+}
---
+++ SnowboardFighting/Assets/Snowboarder.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowboarder : MonoBehaviour {
+    float x;
+	public float thrust = 10;
+	public float jumpThrust = 1000;
+	public float torque = 300;
+    private int sensitivity = 10;
+	public Rigidbody rb;
+	public GameObject snowboard;
+	//Camera cam;
+
+
+	// Use this for initialization
+	void Start () {
+        x = Input.mousePosition.x;
+		rb = GetComponent<Rigidbody> ();
+	//	cam.transform.position = 
+    }
+
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        x = Input.mousePosition.x - x;
+
+        transform.Rotate(Vector3.up * x);
+    }*/
+    // Update is called once per frame
+    void Update () {
+		rb.isKinematic = false;
+       // Pressing 'A' will move to the left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime*5);
+          //  transform.Rotate(Vector3.up * 1 );
+        }
+        // Pressing 'D' will move to the right
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime*5);
+            //transform.Rotate(Vector3.up * -1 );
+        }
+
+		if (Input.GetKey (KeyCode.W)) {
+			rb.AddRelativeForce (Vector3.forward * thrust);
+
+		} 
+
+		if (Input.GetKey (KeyCode.S)) {
+			Debug.Log ("velocity: " + rb.velocity.magnitude);
+			float stop = (rb.velocity.magnitude / Time.deltaTime);//20;//rb.velocity.magnitude;
+			Debug.Log ("stop: " + stop);
+			rb.AddRelativeForce (Vector3.forward * -stop);
+			rb.drag +=0.02f;
+			//rb.isKinematic = true;
+		} else {
+			rb.drag = 0;
+		}
+
+       if (Input.GetAxis("Mouse X") != 0)
+        {
+            //transform.eulerAngles.y += turnSpeed * Time.deltaTime;
+            var x = Input.GetAxis("Mouse X") * sensitivity;      //The above rotates this object at the speed of "speed" degrees per second in the direction pressed.
+            transform.eulerAngles += new Vector3(0, x, 0);
+        }
+		
+		/*if (Input.GetAxis ("Mouse Y") != 0) {
+			Debug.Log ("y axis: " + Input.GetAxis ("Mouse Y"));
+			var y = Input.GetAxis ("Mouse Y") * sensitivity;
+			transform.eulerAngles += new Vector3 (y, 0, 0);
+			rb.AddRelativeTorque (Vector3.left * torque * y);
+		}*/
+        
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			Debug.Log ("Jump");
+			rb.AddRelativeForce (Vector3.up * jumpThrust);
+		}
+        /*  x = Input.mousePosition.x - x;
+
+          transform.Rotate(Vector3.up * Time.deltaTime * x);*/
+		
+    }
+
+	void FixedUpdate() {
+		
+			//rb.AddRelativeForce (Vector3.up * thrust);
+		
+
+	}
+}
